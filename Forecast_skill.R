3f4ba754137aa1ae48d5d3ae17bb37ebc9ffab22
Shaun Harrigan <shaun.p.harrigan@gmail.com>
2019-05-09 16:27:02 +0100

    initial commit

---
+++ Forecast_skill.R
@@ -0,0 +1,23 @@
+library(verification)
+
+# Get observed and forecast data
+
+
+# Create benchmark forecast
+
+verification::brier()
+
+
+
+
+# Hits and misses under Q90 threshold 
+
+
+
+
+# Brier skill score 
+
+
+
+
+# end 
